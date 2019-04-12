d3731664fe082b9b84a2cc224596d9e6bbe7ce52
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-04-12 22:23:02 +0300

    feat: 01-strings-tasks - add function getFirstChar

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -88,7 +88,8 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return (value.charAt(0));
+    // throw new Error('Not implemented');
 }
 
 /**
