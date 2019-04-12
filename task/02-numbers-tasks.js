66655c12f968506d239fe847a628b55392a3a671
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-04-13 01:21:43 +0300

    feat: 02-numbers-tasks - add function getDistanceBetweenPoints

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -76,6 +76,10 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
+    let x = (x2 - x1)*(x2 - x1);
+    let y = (y2 - y1)*(y2 - y1);
+    let result = Math.sqrt(x + y);
+    return result;
     // throw new Error('Not implemented');
 }
 
