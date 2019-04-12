5bf3bbc580b2b02256cd1cf4c9a76456c6fc538b
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-04-12 22:56:18 +0300

    feat: 01-strings-tasks - add function removeFirstOccurrences

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -137,7 +137,8 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+     return str.replace(value,'');
+    // throw new Error('Not implemented');
 }
 
 /**
