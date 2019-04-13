94ac7afb834facec574a352d01ffc57fe51dd1bf
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-04-13 12:39:01 +0300

    feat: 02-numbers-tasks - add function roundToPowerOfTen

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -191,7 +191,8 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
+    pow = Math.pow(10, pow);
+    return Math.round(num / pow)* pow;
 }
 
 /**
