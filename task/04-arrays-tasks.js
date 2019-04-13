8edac6c0f42cc180513de2bed01e7c8a39517356
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-04-13 22:40:22 +0300

    feat: 04-arrays-tasks - add function propagateItemsByPositionIndex

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -277,7 +277,11 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-   throw new Error('Not implemented');
+   let newArr = [];
+   arr.map((item, index) => {
+      return newArr = newArr.concat(Array(index + 1).fill(item));
+   });
+   return newArr;
 }
 
 
